4686e986462f5c532597fb7f9116e2138c136824
Luke <davidson.139@wright.edu>
2025-02-21 16:23:27 +0000

    Added script into fodler

---
+++ task1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#copies the `.vimrc` file from your repository to your user's home directory
+cp .vimrc /home/ubuntu 
+
+
+#installs `Vundle` if `~/.vim/bundle/Vundle.vim` does not exist
+if [! -f ~/.vim/bundle/Vundle.vim]; then 
+	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+
+
+
+
+
+
+
+
