a32b2a58463d46a3655bab64479545e387170dce
Luke <davidson.139@wright.edu>
2025-04-04 21:20:52 +0000

    Finished Script

--- sys-installs/task3.sh
+++ sys-installs/task3.sh
@@ -3,8 +3,20 @@
 if [[ "$EUID" -eq 0 ]]; then
 	if command -v apt-get &> /dev/null; then
  		read -p  'CUDA Toolkit v12.8 is about to be installed. Continue with program? Y/N ' confirm
-		if [[ "$confirm" == "Y" ]]; then
-			echo 'continue'
+		if [[ "$confirm" == "y" ]]; then
+			if [[ ! -f ~/usr/local/cuda ]]; then
+				sudo apt install -y gcc
+				wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-ubuntu2404.pin
+				sudo mv cuda-ubuntu2404.pin /etc/apt/preferences.d/cuda-repository-pin-600
+				wget https://developer.download.nvidia.com/compute/cuda/12.8.1/local_installers/cuda-repo-ubuntu2404-12-8-local_12.8.1-570.124.06-1_amd64.deb
+				sudo dpkg -i cuda-repo-ubuntu2404-12-8-local_12.8.1-570.124.06-1_amd64.deb
+				sudo cp /var/cuda-repo-ubuntu2404-12-8-local/cuda-*-keyring.gpg /usr/share/keyrings/
+				sudo apt-get update
+				sudo apt-get -y install cuda-toolkit-12-8
+			else
+				echo 'This program is already installed on your system.'
+				exit 1
+			fi
 		else
 			echo 'Exiting Program'
 			exit 1
